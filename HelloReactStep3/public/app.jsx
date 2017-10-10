526e166f9fd31f049a3cd080dd58b3be76333962
Gary Thomas <gary@mlbassoc.com>
2017-10-10 13:23:55 +0700

    Add form to capture some user input

--- HelloReactStep3/public/app.jsx
+++ HelloReactStep3/public/app.jsx
@@ -5,12 +5,21 @@ var Greeter = React.createClass({
       message: 'Default message'
     };
   },
+  onButtonClick: function (e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    alert(name);
+  },
   render: function () {
     var {name, message} = this.props;
     return (
       <div>
         <h1>Hello {name} (using components)</h1>
         <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
