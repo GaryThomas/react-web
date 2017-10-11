9d68ef77099b03bb0e7f3f99422bb1422647f5f4
Gary Thomas <gary@mlbassoc.com>
2017-10-11 08:16:08 +0700

    Complete separating components - obvious why webpack include paths will be a good idea

---
+++ HelloReactStep6/public/components/Greeter.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('./GreeterForm');
+
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'REACT',
+      message: 'Default message'
+    };
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name,
+      message: this.props.message,
+    };
+  },
+  handleUpdates: function (updates) {
+    this.setState(updates);
+  },
+  render: function () {
+    var {name, message} = this.state;
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onUpdates={this.handleUpdates}/>
+      </div>
+    );
+  }
+});
+
+module.exports = Greeter;
