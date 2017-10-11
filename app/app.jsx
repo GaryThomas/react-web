b8b9f48d7b149598c588fa5946cd1ee4ac5bf30b
Gary Thomas <gary@mlbassoc.com>
2017-10-11 16:43:36 +0700

    Base setup using routes (only '/' defined)

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var BoilerPlate = require('BoilerPlate');
 
 ReactDOM.render(
-  <BoilerPlate name="React World"/>,
+  <Router history={hashHistory}>
+    <Route path="/" component={BoilerPlate}>
+
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
