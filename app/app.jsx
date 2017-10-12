d4f397f385625cce090f844667ecf9e64411a1e6
Gary Thomas <gary@mlbassoc.com>
2017-10-12 16:54:48 +0700

    Add more routing examples, actual links inside of Nav component

--- app/app.jsx
+++ app/app.jsx
@@ -4,11 +4,13 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Components/Main');
 var Weather = require('Components/Weather');
 var About = require('Components/About');
+var Examples = require('Components/Examples');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path="about" component={About} />
+      <Route path="examples" component={Examples} />
       <IndexRoute component={Weather}/>
     </Route>
   </Router>,
