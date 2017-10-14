0b3ff466afe7abac4b1bcb4254b8109b3525f800
Gary Thomas <gary@mlbassoc.com>
2017-10-14 07:33:21 +0700

    Scaffold new Weather components

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,10 +1,14 @@
 var React = require('react');
+var WeatherForm = require('Components/WeatherForm');
+var WeatherMessage  = require('Components/WeatherMessage');
 
 var Weather = React.createClass({
   render: function () {
     return (
       <div>
         <h2>Weather Component</h2>
+        <WeatherForm />
+        <WeatherMessage />
       </div>
     );
   }
