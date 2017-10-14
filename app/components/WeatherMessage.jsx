56f74df9a876d7d6eb7c2f4662927a6349885e60
Gary Thomas <gary@mlbassoc.com>
2017-10-14 13:59:06 +0700

    Start filling in Weather functionality

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function () {
+    var {temp, location} = this.props;
     return (
       <div>
         <h2>WeatherMessage  Component</h2>
+        The temperature in {location} is {temp}
       </div>
     );
   }
