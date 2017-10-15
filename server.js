7adf9595a6a8e48098b3efdfce477e5856b17e55
Gary Thomas <gary@mlbassoc.com>
2017-10-15 15:13:41 +0700

    Fix problem with https->http redirect code on localhost server

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var port = process.env.PORT || 3000;
 // Redirect any https traffic to http (since openWeatherMap doesn't handle it for free accounts)
 
 app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-      next();
-    } else {
+    if (req.headers['x-forwarded-proto'] === 'https') {
       res.redirect('http://' + res.hostname + res.url);
+    } else {
+      next();
     }
 });
 
