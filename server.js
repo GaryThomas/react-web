ff384c333139112b4c4a0b1941b28153e57330bd
Gary Thomas <gary@mlbassoc.com>
2017-10-15 14:33:36 +0700

    Maybe fix syntax error?

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var port = process.env.PORT || 3000;
 
 // Redirect any https traffic to http (since heroku doesn't handle it)
 
-app.use(function (req, res, next)) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-      next();
-    } else {
-      res.redirect('http://' + res.hostname + res.url);
-    }
-};
+// app.use(function (req, res, next)) {
+//     if (req.headers['x-forwarded-proto'] === 'http') {
+//       next();
+//     } else {
+//       res.redirect('http://' + res.hostname + res.url);
+//     }
+// };
 
 app.use(express.static('public'));
 app.listen(port, function () {
