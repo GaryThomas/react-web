ee5297bb25f14b893926f3001e1b8f99866af72b
Gary Thomas <gary@mlbassoc.com>
2017-10-15 14:16:42 +0700

    Prepare to run via heroku

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 3000;
+
+// Redirect any https traffic to http (since heroku doesn't handle it)
+
+app.use(function (req, res, next)) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+      res.redirect('http://' + res.hostname + res.url);
+    }
+};
 
 app.use(express.static('public'));
-app.listen(3000, function () {
-  console.log('Express server running');
+app.listen(port, function () {
+  console.log('Express server running on port #' + port);
 });
