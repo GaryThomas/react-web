482b649501604490c748162461a7800ffdbbdc8b
Gary Thomas <gary@mlbassoc.com>
2017-10-15 15:02:04 +0700

    Try to keep bundle.js to make heroku happy

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// Redirect any https traffic to http (since heroku doesn't handle it)
+// Redirect any https traffic to http (since openWeatherMap doesn't handle it for free accounts)
 
-// app.use(function (req, res, next)) {
-//     if (req.headers['x-forwarded-proto'] === 'http') {
-//       next();
-//     } else {
-//       res.redirect('http://' + res.hostname + res.url);
-//     }
-// };
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+      res.redirect('http://' + res.hostname + res.url);
+    }
+};
 
 app.use(express.static('public'));
 app.listen(port, function () {
