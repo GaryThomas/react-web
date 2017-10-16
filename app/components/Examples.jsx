29ed3aa45f19fa335bf7f0461c0de38f18d5baa1
Gary Thomas <gary@mlbassoc.com>
2017-10-16 13:11:49 +0700

    Add some simple styling for 'examples' and set up auto-search link route example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function () {
     return (
       <div>
         <h1 className="text-center">Examples</h1>
-        <p>Some example text goes here</p>
+        <p>Here are a few examples to try:</p>
+        <ol>
+          <li><Link to="/?location=Denver">Denver, Colorado</Link></li>
+        </ol>
       </div>
     );
   }
