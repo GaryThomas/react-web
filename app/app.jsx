f7d7ea9346c6520881f75ca51a3e0e05fc945d9c
Gary Thomas <gary@mlbassoc.com>
2017-10-20 08:09:45 +0700

    Basic boilerplate - React + Foundation

--- app/app.jsx
+++ app/app.jsx
@@ -2,9 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Components/Main');
-var Weather = require('Components/Weather');
-var About = require('Components/About');
-var Examples = require('Components/Examples');
 
 // Load foundation CSS & start foundation framework
 require('style-loader!css-loader!foundation-sites/dist/foundation.min.css');
@@ -16,9 +13,6 @@ require('style-loader!css-loader!sass-loader!Styles/app.scss');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="about" component={About} />
-      <Route path="examples" component={Examples} />
-      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,11 +1,10 @@
 var React = require('react');
-var Nav = require('Components/Nav');
 
 var Main = React.createClass({
   render: function () {
     return (
       <div>
-        <Nav />
+        <h1>Main Application</h1>
         <div className="row>">
           <div className="columns medium-6 large-4 small-centered">
             {this.props.children}
