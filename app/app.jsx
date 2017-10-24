4a1e25d987884235444a0a0ded1993153fb02623
Gary Thomas <gary@mlbassoc.com>
2017-10-24 14:52:34 +0700

    Add component top-level framework

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Components/Main');
+var Timer = require('Components/Timer');
+var CountDown = require('Components/CountDown');
 
 // Load foundation CSS & start foundation framework
 require('style-loader!css-loader!foundation-sites/dist/foundation.min.css');
@@ -13,6 +15,8 @@ require('style-loader!css-loader!sass-loader!Styles/app.scss');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="counter" component={CountDown}/>
+      <IndexRoute component={Timer}/>
     </Route>
   </Router>,
   document.getElementById('app')
---
+++ app/components/CountDown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var CountDown = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1> Count Down </h1>
+      </div>
+    );
+  }
+});
+
+module.exports = CountDown;
