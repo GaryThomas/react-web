44f051e788e199837665bf92816b2ab8d67942ce
Gary Thomas <gary@mlbassoc.com>
2017-10-25 12:16:31 +0700

    Fix formatting of time strings

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  formatNumber: function (num, digits) {
+    var str = num.toString();
+    var pad = digits - str.length;
+    while (pad > 0) {
+      pad -= 1;
+      str = '0' + str;
+    }
+    return str;
+  },
   formatSeconds: function (time) {
     var sec = time % 60;
     var min = Math.floor(time / 60);
-    return min + ':' + sec;
+    return this.formatNumber(min, 2) + ':' + this.formatNumber(sec, 2);
   },
   render: function() {
     return (
